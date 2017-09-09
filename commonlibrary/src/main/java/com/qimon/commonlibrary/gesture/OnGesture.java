357c92ddb1cbcdf8d81e765cfe6685b5503c87c4
zeffect <625417632@qq.com>
2017-09-09 22:16:02 +0800

    手势监听,延时点击

---
+++ commonlibrary/src/main/java/com/qimon/commonlibrary/gesture/OnGesture.java
@@ -0,0 +1,73 @@
+package com.qimon.commonlibrary.gesture;
+
+import android.view.MotionEvent;
+
+/**
+ * Created by Administrator on 2017/9/9.
+ */
+
+public abstract class OnGesture {
+    /**
+     * 上滑
+     **/
+    public void onTop() {
+    }
+
+    /**
+     * 下滑
+     */
+    public void onBottom() {
+    }
+
+    /***左滑**/
+    public void onLeft() {
+    }
+
+    /**
+     * 右滑
+     **/
+    public void onRight() {
+    }
+
+    /**
+     * 按下
+     **/
+    public void onDown() {
+    }
+
+    /**
+     * 抬起
+     **/
+    public void onUp() {
+    }
+
+    /**
+     * 移动
+     **/
+    public void onMove(MotionEvent event1, MotionEvent event2) {
+    }
+
+    /**
+     * 单击
+     **/
+    public void onSingleUp() {
+    }
+
+    /**
+     * 双击
+     **/
+    public void onDoubleUp() {
+    }
+
+    /**
+     * 按下
+     */
+    public void onPressed() {
+    }
+
+    /**
+     * 长按
+     */
+    public void onLong() {
+    }
+}
--- commonlibrary/src/main/java/com/qimon/commonlibrary/gesture/ZGesture.kt
+++ commonlibrary/src/main/java/com/qimon/commonlibrary/gesture/ZGesture.kt
@@ -1,63 +1,80 @@
 package com.qimon.commonlibrary.gesture
 
-import android.util.Log
+import android.content.Context
+import android.view.GestureDetector
 import android.view.MotionEvent
 
 /**
  * Created by Administrator on 2017/9/8.
+ * 对手势事件进行封装，有上下左右，单击，双击，长按，move,pressed,up
+ *
+ * 如果发现onFling没有回调，请设置View.setLongClickable(true)
  */
-class ZGesture(gesture: OnGesture?) {
+class ZGesture(context: Context, gesture: OnGesture?) : GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+    override fun onShowPress(p0: MotionEvent?) {
+        mGestureInterface?.onPressed()
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onDoubleTap(p0: MotionEvent?): Boolean {
+        mGestureInterface?.onDoubleUp()
+        mGestureInterface?.onUp()
+        return true
+    }
+
+    override fun onDown(p0: MotionEvent?): Boolean {
+        mGestureInterface?.onDown()
+        return true
+    }
+
+    override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        val tempX = p1?.x ?: 0f.minus(p0?.x ?: 0f)
+        val tempY = p1?.y ?: 0f.minus(p0?.y ?: 0f)
+        if (Math.abs(tempX) > Math.abs(tempY)) {
+            if (tempX > 0) mGestureInterface?.onRight() else mGestureInterface?.onLeft()
+        } else {
+            if (tempY > 0) mGestureInterface?.onBottom() else mGestureInterface?.onTop()
+        }
+        mGestureInterface?.onUp()
+        return true
+    }
+
+    override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        mGestureInterface?.onMove(p0, p1)
+        return true
+    }
+
+    override fun onLongPress(p0: MotionEvent?) {
+        mGestureInterface?.onLong()
+        mGestureInterface?.onUp()
+    }
+
+    override fun onDoubleTapEvent(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onSingleTapConfirmed(p0: MotionEvent?): Boolean {
+        mGestureInterface?.onSingleUp()
+        mGestureInterface?.onUp()
+        return true
+    }
+
     private val TAG = ZGesture::class.java.name
     private val mGestureInterface by lazy { gesture }
     private val DOUBLE_TIME = 100
-    /**
-     * 用这个来标记是单击还是双击
-     * mIndex%2==0代表单击否则双击
-     * */
-    private var mIndex: Int = 1.unaryMinus()
 
-    private var mLastTime: Long = 0L
+    private val mContext by lazy { context }
 
     /**
      * 通过这个接口，得到触摸事件
      * */
     fun onTouchEvent(event: MotionEvent): Boolean {
-        return doEvent(event)
+        return mGestureDetector.onTouchEvent(event)
     }
 
-    private var downX = 0f
-    private var downY = 0f
+    private val mGestureDetector: GestureDetector by lazy { GestureDetector(mContext, this@ZGesture).apply { this.setOnDoubleTapListener(this@ZGesture) } }
 
-    private fun doEvent(event: MotionEvent): Boolean {
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                Log.e(TAG, "down:${event.x},${event.y}")
-                downX = event.x
-                downY = event.y
-                if (mIndex < (0)) mIndex = 0//说明没有点击过
-                mIndex.inc()
-                if (mIndex.rem(2) == 1) mLastTime = System.currentTimeMillis()
-            }
-            MotionEvent.ACTION_MOVE -> {
-            }
-            MotionEvent.ACTION_UP -> {
-                //单双击的漂移，最多10dp
-                Log.e(TAG, "当前index:$mIndex")
-                if (Math.abs(downX.minus(event.x)) < 10 && Math.abs(downY.minus(event.y)) < 10) {
-                    if (mIndex.rem(2) == 0) mGestureInterface?.onDoubleUp()
-                    else mGestureInterface?.onSingleUp()
-                } else {
-
-                }
-            }
-        }
-        return true
-    }
-
-
-    interface OnGesture {
-        fun onUp() {}
-        fun onSingleUp() {}
-        fun onDoubleUp() {}
-    }
 }
