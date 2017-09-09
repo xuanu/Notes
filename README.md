bd2656f2ece4f9d13b63f304cf353b88dc0cb78b
zeffect <625417632@qq.com>
2017-09-09 22:17:48 +0800

    手势监听,延时点击

--- README.md
+++ README.md
@@ -3,4 +3,17 @@ android开发笔记
 
 1. **[安卓性能优化](https://github.com/xuanu/Notes/blob/master/Notes/optimize/%E5%AE%89%E5%8D%93%E4%B8%AD%E7%9A%84%E6%80%A7%E8%83%BD%E4%BC%98%E5%8C%96.md)**;  
 2. **[设计实现简单的下载功能](https://github.com/xuanu/Notes/blob/master/Notes/download/%E8%AE%BE%E8%AE%A1%E5%AE%9E%E7%8E%B0%E4%B8%80%E4%B8%AA%E7%AE%80%E5%8D%95%E4%B8%8B%E8%BD%BD%E5%8A%9F%E8%83%BD.md)**
-3. **[MediaPlay的简单封装，支持顺序播放多个音频。](https://github.com/xuanu/Notes/blob/master/Notes/MediaPlayer%E7%9A%84%E7%AE%80%E5%8D%95%E5%B0%81%E8%A3%85.md)**  
+3. **[MediaPlay的简单封装，支持顺序播放多个音频。](https://github.com/xuanu/Notes/blob/master/Notes/MediaPlayer%E7%9A%84%E7%AE%80%E5%8D%95%E5%B0%81%E8%A3%85.md)**
+4. [手势监听的封装，上下左右，单击，双击，长按，move,down,up,pressed](https://github.com/xuanu/Notes/blob/master/commonlibrary/src/main/java/com/qimon/commonlibrary/gesture/ZGesture.kt);
+> 如果onFling不回调，设置View.setLongClickable(true)
+```
+//使用方法与GestureDetector一致，仅对其进行封装，方便使用
+ZGesture(context,OnGesture).onTouchEvent(event)
+```
+5. [点击间隔，短时间内不重复响应](https://github.com/xuanu/Notes/blob/master/commonlibrary/src/main/java/com/qimon/commonlibrary/gesture/OnClickFastListener.java)
+```
+View.setOnClickListener(new OnClickFastListener(time){
+ public  void onFastClick(View v){
+ }
+});
+```
--- app/src/main/java/cn/zeffect/notes/MainActivity.java
+++ app/src/main/java/cn/zeffect/notes/MainActivity.java
@@ -7,15 +7,68 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.qimon.commonlibrary.gesture.OnGesture;
 import com.qimon.commonlibrary.gesture.ZGesture;
+import com.squareup.haha.perflib.Main;
+
+import org.jetbrains.annotations.NotNull;
 
 import cn.zeffect.notes.down.DownActivity;
 import cn.zeffect.notes.memoryleak.MemoryLeakActivity;
 import cn.zeffect.notes.utils.IntentUtils;
+import cn.zeffect.notes.utils.L;
 
-public class MainActivity extends Activity implements View.OnClickListener, ZGesture.OnGesture {
+public class MainActivity extends Activity implements View.OnClickListener {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private View mView;
-    private ZGesture mGesture = new ZGesture(this);
+    private ZGesture mGesture = new ZGesture(this, new OnGesture() {
+        @Override
+        public void onTop() {
+            Snackbar.make(mView, "上", Snackbar.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onBottom() {
+            Snackbar.make(mView, "下", Snackbar.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onLeft() {
+            Snackbar.make(mView, "左", Snackbar.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onRight() {
+            Snackbar.make(mView, "右", Snackbar.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onSingleUp() {
+            Snackbar.make(mView, "单击", Snackbar.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onDoubleUp() {
+            Snackbar.make(mView, "双击", Snackbar.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onDown() {
+        }
+
+        @Override
+        public void onUp() {
+        }
+
+        @Override
+        public void onMove(MotionEvent event1, MotionEvent event2) {
+        }
+
+        @Override
+        public void onLong() {
+            Snackbar.make(mView, "长按", Snackbar.LENGTH_SHORT).show();
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +77,7 @@ public class MainActivity extends Activity implements View.OnClickListener, ZGes
         mView = findViewById(R.id.root_layout);
         findViewById(R.id.goMemory).setOnClickListener(this);
         findViewById(R.id.gotoDown).setOnClickListener(this);
+        findViewById(R.id.gotoPlay).setLongClickable(true);
         findViewById(R.id.gotoPlay).setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View pView, MotionEvent pEvent) {
@@ -42,20 +96,5 @@ public class MainActivity extends Activity implements View.OnClickListener, ZGes
                 IntentUtils.goTarget(MainActivity.this, DownActivity.class);
                 break;
         }
-    }
-
-    @Override
-    public void onUp() {
-
-    }
-
-    @Override
-    public void onSingleUp() {
-        Snackbar.make(mView, "单击", Snackbar.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onDoubleUp() {
-        Snackbar.make(mView, "双击", Snackbar.LENGTH_SHORT).show();
     }
 }
